5d633fafcc8036c63c55093c15f8801f980a1a9a
alanafdz88 <alanafernandez0120@gmail.com>
2026-02-23 11:19:34 -0600

    commit : categoría y su listado listo y visible

---
+++ src/main/java/com/tienda2/repository/CategoriaRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda2.repository;
+
+import com.tienda2.domain.Categoria;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+    
+    // Método para obtener todas las categorías activas
+    public List<Categoria> findByActivoTrue();
+}
