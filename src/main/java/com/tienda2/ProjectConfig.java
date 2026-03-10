fe6c571f2f902592e9a97eb28bdc4ca7f3b150f2
alanafdz88 <alanafernandez0120@gmail.com>
2026-03-09 20:36:39 -0600

    commit 8: usuarios string security

--- src/main/java/com/tienda2/ProjectConfig.java
+++ src/main/java/com/tienda2/ProjectConfig.java
@@ -1,16 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tienda2;
 
-
 import java.util.Locale;
-import org.springframework.boot.autoconfigure.web.WebProperties.LocaleResolver;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,20 +25,32 @@ import org.thymeleaf.templatemode.TemplateMode;
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
 
-/* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
-@Override
-public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/").setViewName("index");
-    registry.addViewController("/ejemplo2").setViewName("ejemplo2");
-    registry.addViewController("/multimedia").setViewName("multimedia");
-    registry.addViewController("/iframes").setViewName("iframes");
-    registry.addViewController("/login").setViewName("login");
-    registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
-}
+    /* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/ejemplo2").setViewName("ejemplo2");
+        registry.addViewController("/multimedia").setViewName("multimedia");
+        registry.addViewController("/iframes").setViewName("iframes");
+        registry.addViewController("/login").setViewName("login"); //redireccionamiento para poder ingresar al login
+        registry.addViewController("/acceso_denegado").setViewName("acceso_denegado");
+        registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
+    }
 
+    /* El siguiente método se utilizar para publicar en la nube, independientemente  */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    }
 
- @Bean
-    public org.springframework.web.servlet.LocaleResolver localeResolver() {
+    @Bean
+    public LocaleResolver localeResolver() {
         var slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.getDefault());
         slr.setLocaleAttributeName("session.current.locale");
@@ -43,43 +58,106 @@ public void addViewControllers(ViewControllerRegistry registry) {
         return slr;
     }
 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registro) {
+        registro.addInterceptor(localeChangeInterceptor());
+    }
 
+    //Bean para poder acceder a los messages.properties en código...
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 
+    public static final String[] PUBLIC_URLS = {
+        "/", "/index", "/fav/**", "/carrito/**", "/consultas/**", "/registro/**",
+        "/js/**", "/webjars/**", "/login", "/acceso_denegado"
+    };
 
-/* El siguiente método se utilizar para publicar en la nube, independientemente */
-@Bean
-public SpringResourceTemplateResolver templateResolver_0() {
-    SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
-  resolver.setPrefix("classpath:/templates/");
-    resolver.setSuffix(".html");
-    resolver.setTemplateMode(TemplateMode.HTML);
-    resolver.setOrder(0);
-    resolver.setCheckExistence(true);
-    return resolver;
-    
-    
+    public static final String[] ADMIN_URLS = {
+        "/producto/nuevo", "/producto/guardar", "/producto/modificar/**", "/producto/eliminar/**",
+        "/categoria/nuevo", "/categoria/guardar", "/categoria/modificar/**", "/categoria/eliminar/**",
+        "/usuario/nuevo", "/usuario/guardar", "/usuario/modificar/**", "/usuario/eliminar/**"
+    };
+
+    public static final String[] ADMIN_OR_VENDEDOR_URLS = {
+        "/producto/listado", "/categoria/listado", "/usuario/listado"
+    };
+
+    public static final String[] USUARIO_URLS = {
+        "/facturar/carrito"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                .requestMatchers(PUBLIC_URLS).permitAll()
+                .requestMatchers(ADMIN_URLS).hasRole("ADMIN")
+                .requestMatchers(ADMIN_OR_VENDEDOR_URLS).hasAnyRole("ADMIN", "VENDEDOR")
+                .requestMatchers(USUARIO_URLS).hasRole("USUARIO")
+                .anyRequest().authenticated()
+        ).formLogin(form -> form // Configuración de formulario de login
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error=true")
+                .permitAll()
+        ).logout(logout -> logout // Configuración de logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID") //se borran cookine once logged out
+                .permitAll()
+        ).exceptionHandling(exceptions -> exceptions // Manejo de excepciones
+                .accessDeniedPage("/acceso_denegado")
+        ).sessionManagement(session -> session // Configuración de sesiones
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false)
+        );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //Este método será reemplazado la siguiente semana
+    @Bean
+    public UserDetailsService users(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder()
+                .username("juan")
+                .password(passwordEncoder.encode("123"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails sales = User.builder()
+                .username("rebeca")
+                .password(passwordEncoder.encode("456"))
+                .roles("VENDEDOR")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("pedro")
+                .password(passwordEncoder.encode("789"))
+                .roles("USUARIO") // Consistent con tu configuración
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, sales, user);
+    }
+
 }
 
-@Bean
-public LocaleChangeInterceptor localeChangeInterceptor() {
-    var lci = new LocaleChangeInterceptor();
-    lci.setParamName("lang");
-    return lci;
-}
 
-@Override
-public void addInterceptors(InterceptorRegistry registro) {
-    registro.addInterceptor(localeChangeInterceptor());
-}
 
-//Bean para poder acceder a los messages.properties en código...
-@Bean("messageSource")
-public MessageSource messageSource() {
-    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-    messageSource.setBasenames("messages");
-    messageSource.setDefaultEncoding("UTF-8");
-    return messageSource;
-}
 
-}
